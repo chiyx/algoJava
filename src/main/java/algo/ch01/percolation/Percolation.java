5a23c4b60b7c2c5cf364f32926e284906f81e73e
chiyx <chiyxme@outlook.com>
2016-09-10 14:28:00 +0800

    algo ch01: Percolation test

--- src/main/java/algo/ch01/percolation/Percolation.java
+++ src/main/java/algo/ch01/percolation/Percolation.java
@@ -1,28 +1,113 @@
 package algo.ch01.percolation;
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 /**
  * algs4-week-homework-percolation
  * Created by chiyx on 2016/9/9.
  */
 public class Percolation {
 
-    public Percolation(int n) {
+    private int n;
 
+    // the grid, true for open
+    private boolean[][] grid;
+
+    // for percolates judge, has top adn bottom virSite
+    private WeightedQuickUnionUF uf1;
+
+    // for full judge, has top virSite
+    private WeightedQuickUnionUF uf2;
+
+    private int virTopSiteIdx;
+
+    private int virBotSiteIdx;
+
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n <= 0");
+        }
+        this.n = n;
+        this.grid = new boolean[n][n];
+        int numOfSite = n * n;
+        this.uf1 = new WeightedQuickUnionUF(numOfSite + 2);
+        this.uf2 = new WeightedQuickUnionUF(numOfSite + 1);
+        virTopSiteIdx = numOfSite; // the range of uf1 is [0, n * n + 1]
+        virBotSiteIdx = numOfSite + 1;
+        // link top row to virTopSite, do for 2 uf
+        for (int col = 1; col <= n; col++) {
+            uf1.union(toUfIndex(1, col), virTopSiteIdx);
+            uf2.union(toUfIndex(1, col), virTopSiteIdx);
+        }
+        // link bot row to virBotSite, just do for uf1
+        for (int col = 1; col <= n; col++) {
+            uf1.union(toUfIndex(n, col), virBotSiteIdx);
+        }
     }
 
     public void open(int i, int j) {
-
+        assetsBounds(i, j);
+        if (!isOpen(i, j)) {
+            // mark open
+            grid[i - 1][j - 1] = true;
+            int curIdx = toUfIndex(i, j);
+            // connect to neighbor open site
+            // 1. neighbor of previous row
+            unionOpenNeighbor(curIdx, i - 1, j);
+            // 2. neighbor of next col
+            unionOpenNeighbor(curIdx, i, j + 1);
+            // 3. neighbor of next row
+            unionOpenNeighbor(curIdx, i + 1, j);
+            // 4. neighbor of previous col
+            unionOpenNeighbor(curIdx, i, j - 1);
+        }
     }
 
     public boolean isOpen(int i, int j) {
-        return false;
+        assetsBounds(i, j);
+        return grid[i - 1][j - 1];
     }
 
     public boolean isFull(int i, int j) {
-        return false;
+        assetsBounds(i, j);
+        // open && connected to virTopSite, use uf2 to judge
+        return isOpen(i, j) && uf2.connected(toUfIndex(i, j), virTopSiteIdx);
     }
 
     public boolean percolates() {
-        return false;
+        return uf1.connected(virTopSiteIdx, virBotSiteIdx);
+    }
+
+    private void unionOpenNeighbor(int curIdx, int neighborI, int neighborJ) {
+        int neighborIdx = toUfIndex(neighborI, neighborJ);
+        if (isInBoundsAndOpen(neighborI, neighborJ)) {
+            uf1.union(curIdx, neighborIdx);
+            uf2.union(curIdx, neighborIdx);
+        }
+    }
+
+    /**
+     * to uf index
+     *
+     * @param i row of grid
+     * @param j col of grid
+     * @return index of uf
+     */
+    private int toUfIndex(int i, int j) {
+        return n * (i - 1) + (j - 1);
+    }
+
+    private boolean isInBoundsAndOpen(int row, int col) {
+        return isInBounds(row) && isInBounds(col) && isOpen(row, col);
+    }
+
+    private void assetsBounds(int row, int col) {
+        if (!isInBounds(row) || !isInBounds(col)) {
+            throw new IndexOutOfBoundsException("row or col must in range [1, n]");
+        }
+    }
+
+    private boolean isInBounds(int i) {
+        return i >= 1 && i <= n;
     }
 }
--- src/main/java/algo/ch01/percolation/PercolationStats.java
+++ src/main/java/algo/ch01/percolation/PercolationStats.java
@@ -1,5 +1,7 @@
 package algo.ch01.percolation;
 
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 /**
@@ -12,35 +14,80 @@ public class PercolationStats {
 
     private int trials;
 
+    private int n;
+
+    private double mean;
+
+    private double stddev;
+
     public PercolationStats(int n, int trials) {
         if (n <= 0 || trials <= 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("n or trials <= 0 ");
         }
         this.trials = trials;
+        this.n = n;
         this.thresholds = new double[trials];
+        percolationRun();
+        this.mean = StdStats.mean(thresholds);
+        this.stddev = StdStats.stddev(thresholds);
     }
 
     public double mean() {
-        return StdStats.mean(thresholds);
+        return mean;
     }
 
     public double stddev() {
-        return StdStats.stddev(thresholds);
+        return stddev;
     }
 
     public double confidenceLo() {
-        double u = mean();
-        double b = stddev();
+        double u = mean;
+        double b = stddev;
         return u - (1.96 * b) / Math.sqrt(trials);
     }
 
     public double confidenceHi() {
-        double u = mean();
-        double b = stddev();
+        double u = mean;
+        double b = stddev;
         return u + (1.96 * b) / Math.sqrt(trials);
     }
 
-    public static void main(String[] args) {
-
+    private void percolationRun() {
+        int numOfSite = n * n;
+        // run trial times
+        for (int i = 0; i < trials; i++) {
+            int numOfOpenSite = 0;
+            Percolation percolation = new Percolation(n);
+            while (!percolation.percolates()) {
+                int randI = StdRandom.uniform(n) + 1;
+                int randJ = StdRandom.uniform(n) + 1;
+                if (!percolation.isOpen(randI, randJ)) {
+                    percolation.open(randI, randJ);
+                    numOfOpenSite++;
+                }
+            }
+            thresholds[i] = numOfOpenSite / (double) numOfSite;
+        }
     }
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            StdOut.println("You must pass two arguments");
+            return;
+        }
+        int n = Integer.parseInt(args[0]);
+        int trails = Integer.parseInt(args[1]);
+        PercolationStats stats = new PercolationStats(n, trails);
+        double mean = stats.mean();
+        double stddev = stats.stddev();
+        StdOut.println("mean                    = " + mean);
+        StdOut.println("stddev                  = " + stddev);
+        StdOut.println("95% confidence interval = "
+                +
+                stats.confidenceLo()
+                + ", "
+                +
+                stats.confidenceHi());
+    }
+
 }
