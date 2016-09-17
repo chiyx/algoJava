2b92581c3a0c0cde06b7dcb1e6d2b28c6dbbcf63
chiyx <chiyxme@outlook.com>
2016-09-17 23:59:45 +0800

    algo ch02: queues,, random queues api

---
+++ src/main/java/algo/ch02/queue/RandomizedQueue.java
@@ -0,0 +1,40 @@
+package algo.ch02.queue;
+
+import java.util.Iterator;
+
+/**
+ * Created by chiyx on 2016/9/17.
+ */
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    private int size;
+
+    public RandomizedQueue() {
+
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void enqueue(Item item) {
+
+    }
+
+    public Item dequeue() {
+        return null;
+    }
+
+    public Item sample() {
+       return null;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
